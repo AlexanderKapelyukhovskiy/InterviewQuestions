11251a69b758f7c880a73b798073d0c0e824bc06
AlexanderKapelyukhovskiy <alkapa2005@gmail.com>
2013-05-14 18:24:05 +0300

    41. 6/5	Prints out an unsigned long in decimal

---
+++ InterviewQuestions.PrintLongProblem.Tests/ProblemSolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+
+namespace InterviewQuestions.PrintLongProblem.Tests
+{
+	[TestFixture]
+	public class ProblemSolutionTests
+	{
+		[Test]
+		public void PrintLong1Test()
+		{
+			ProblemSolution.PrintLong1(123141413240, v => Trace.Write(" " + v + " "));
+		}
+
+		[Test]
+		public void PrintLong2Test()
+		{
+			ProblemSolution.PrintLong2(123141413240, v => Trace.Write(" " + v + " "));
+		}
+	}
+}
---
+++ InterviewQuestions.PrintLongProblem/ProblemSolution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace InterviewQuestions.PrintLongProblem
+{
+	/// <summary>
+	/// Given only putchar (no sprintf, itoa, etc.) write a routine putlong that prints
+	/// out an unsigned long in decimal.
+	/// </summary>
+	public class ProblemSolution
+	{
+		public static void PrintLong1(long value, Action<int> print)
+		{
+			if (value < 10)
+			{
+				print((int)value);
+			}
+			else
+			{
+				PrintLong1(value / 10, print);
+				print((int)(value % 10));
+			}
+		}
+
+		public static void PrintLong2(long value, Action<int> print)
+		{
+			long i = 10;
+			while (i < value)
+			{
+				i *= 10;
+			}
+			i /= 10;
+
+			while (i >= 10)
+			{
+				long r = (value / i);
+				print((int)r);
+				value = value - r * i;
+				i /= 10;
+			}
+			print((int)value);
+		}
+	}
+}
