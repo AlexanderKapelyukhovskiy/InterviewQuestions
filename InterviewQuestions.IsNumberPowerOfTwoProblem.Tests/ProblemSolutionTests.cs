2c9082ab261052867ea4b6e49167187ebb3d5284
AlexanderKapelyukhovskiy <alkapa2005@gmail.com>
2013-05-14 18:32:55 +0300

    42. 6/6	Number is a power of 2

---
+++ InterviewQuestions.IsNumberPowerOfTwoProblem.Tests/ProblemSolutionTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace InterviewQuestions.IsNumberPowerOfTwoProblem.Tests
+{
+	[TestFixture]
+	public class ProblemSolutionTests
+	{
+		[Test]
+		[TestCase(2, true)]
+		[TestCase(3, false)]
+		[TestCase(128, true)]
+		[TestCase(234, false)]
+		public void IsPowerOfTwoTest(int x, bool result)
+		{
+			bool r = ProblemSolution.IsPowerOfTwo(x);
+			Assert.That(r, Is.EqualTo(result));
+		}
+	}
+}
---
+++ InterviewQuestions.IsNumberPowerOfTwoProblem/ProblemSolution.cs
@@ -0,0 +1,13 @@
+﻿namespace InterviewQuestions.IsNumberPowerOfTwoProblem
+{
+	/// <summary>
+	/// Give a one-line C expression to test whether a number is a power of 2.
+	/// </summary>
+	public class ProblemSolution
+	{
+		public static bool IsPowerOfTwo(int x)
+		{
+			return (x != 0) && ((x & (x - 1)) == 0);
+		}
+	}
+}
