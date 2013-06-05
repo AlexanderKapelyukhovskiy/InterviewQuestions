1328755b46c2fd0dac588775b71f4f1dc8e86ad0
Alexander Kapelyukhovskiy <alkapa2005@gmail.com>
2013-06-05 14:39:07 +0300

    43. 6/7	Reverse the order of the words

---
+++ InterviewQuestions.ReverseOrderOfWordsProblem.Tests/ProblemSolutionTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+namespace InterviewQuestions.ReverseOrderOfWordsProblem.Tests
+{
+	[TestFixture]
+	public class ProblemSolutionTests
+	{
+		[Test]
+		[TestCase(null,null)]
+		[TestCase("", "")]
+		[TestCase("test", "test")]
+		[TestCase("test1 test2", "test2 test1")]
+		[TestCase(" test1  test2  test3 ", "test3 test2 test1")]
+		public void ReverseOneTest(string input, string expextedResult)
+		{
+			string result = ProblemSolution.ReverseOne(input);
+			Assert.That(result, Is.EqualTo(expextedResult));
+		}
+
+		[Test]
+		[TestCase(null, null)]
+		[TestCase("", "")]
+		[TestCase("test", "test")]
+		[TestCase("test1 test2", "test2 test1")]
+		[TestCase(" test1  t  test2  test3 ", "test3 test2 t test1")]
+		public void ReverseTwoTest(string input, string expextedResult)
+		{
+			string result = ProblemSolution.ReverseTwo(input);
+			Assert.That(result, Is.EqualTo(expextedResult));
+		}
+	}
+}
---
+++ InterviewQuestions.ReverseOrderOfWordsProblem/ProblemSolution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+
+namespace InterviewQuestions.ReverseOrderOfWordsProblem
+{
+	public class ProblemSolution
+	{
+		public static string ReverseOne(string input)
+		{
+			if (string.IsNullOrEmpty(input))
+				return input;
+
+			string[] words = input.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+
+			int length = words.Length;
+			if (length == 1)
+				return input;
+
+			var sb = new StringBuilder(words[length - 1]);
+			for (int i = length - 2; i >= 0; i--)
+			{
+				sb.AppendFormat(" {0}", words[i]);
+			}
+			return sb.ToString();
+		}
+
+		public static string ReverseTwo(string input)
+		{
+			if (string.IsNullOrEmpty(input) || input.LastIndexOf(' ') == -1)
+				return input;
+
+			int end = input.Length - 1;
+			int begin = 0;
+			var sb = new StringBuilder();
+			bool found = false;
+			for (int i = input.Length - 1; i >= 0; i--)
+			{
+				if (input[i] == ' ')
+				{
+					if (end != i)
+					{
+						sb.AppendFormat((found) ? " {0}" : "{0}", input.Substring(i + 1, end - i));
+						found = true;
+					}
+					end = i - 1;
+				}
+				else
+					begin = i;
+			}
+
+			var lastString = input.Substring(begin, end + 1);
+			if (string.IsNullOrEmpty(lastString) == false)
+				sb.AppendFormat(" {0}", lastString);
+			
+			return sb.ToString();
+		}
+	}
+}
