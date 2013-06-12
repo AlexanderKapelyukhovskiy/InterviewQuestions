9d33393dead8833aa22b92338f5d42f7bd0388e6
Alexander Kapelyukhovskiy <alkapa2005@gmail.com>
2013-06-12 18:30:39 +0300

    44. 6/9	Count the number of ones in a 32 bit number

---
+++ InterviewQuestions.FastBitCountingProblem.Tests/ProblemSolutionTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace InterviewQuestions.FastBitCountingProblem.Tests
+{
+	[TestFixture]
+	public class ProblemSolutionTests
+	{
+		[Test]
+		[TestCase(0,0)]
+		[TestCase(1, 1)]
+		[TestCase(2, 1)]
+		[TestCase(3, 2)]
+		public void BitCount1Test(int value, int expextedCount)
+		{
+			int count = ProblemSolution.BitCount1(value);
+			Assert.That(count, Is.EqualTo(expextedCount));
+		}
+
+
+		[Test]
+		[TestCase(0, 0)]
+		[TestCase(1, 1)]
+		[TestCase(2, 1)]
+		[TestCase(3, 2)]
+		public void BitCount2Test(int value, int expextedCount)
+		{
+			int count = ProblemSolution.BitCount2(value);
+			Assert.That(count, Is.EqualTo(expextedCount));
+		}
+	}
+}
---
+++ InterviewQuestions.FastBitCountingProblem/ProblemSolution.cs
@@ -0,0 +1,25 @@
+﻿namespace InterviewQuestions.FastBitCountingProblem
+{
+	public class ProblemSolution
+	{
+		public static int BitCount1(int value)
+		{
+			int count = 0;
+			while (value > 0)
+			{
+				// until all bits are zero
+				if ((value & 1) == 1) // check lower bit
+					count++;
+				value >>= 1; // shift bits, removing lower bit
+			}
+			return count;
+		}
+
+		public static int BitCount2(int i)
+		{
+			i = i - ((i >> 1) & 0x55555555);
+			i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+			return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+		}
+	}
+}
