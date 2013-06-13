5cfd2b1bb5bdd99973f8dc59dfc1181e35eb06f7
Alexander Kapelyukhovskiy <alkapa2005@gmail.com>
2013-06-13 15:47:52 +0300

    45.	7/25	Reverse linked list

---
+++ InterviewQuestions.ReverseLinkedListProblem.Tests/ProblemSolutionTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace InterviewQuestions.ReverseLinkedListProblem.Tests
+{
+	[TestFixture]
+	public class ProblemSolutionTests
+	{
+		[Test]
+		[TestCase(new[] { 1 }, new[] { 1 })]
+		[TestCase(new[] { 1, 2, 3 }, new[] { 3, 2, 1 })]
+		[TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 })]
+		public void ReverseFirstVersionTest(int[] listValues, int[] reversedValues)
+		{
+			var head = CreateLinkedList(listValues);
+			LinkedListItem<int> reversedHead = ProblemSolution<int>.ReverseFirstVersion(head);
+			Assert.IsNotNull(reversedHead);
+			int i = 0;
+			while (reversedHead != null)
+			{
+				Assert.That(reversedHead.Value, Is.EqualTo(reversedValues[i]));
+				reversedHead = reversedHead.Next;
+				i++;
+			}
+		}
+
+		[Test]
+		[TestCase(new[] {1}, new[] {1})]
+		[TestCase(new[] {1, 2, 3}, new[] {3, 2, 1})]
+		[TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new[] {9, 8, 7, 6, 5, 4, 3, 2, 1 })]
+		public void ReverseTest(int[] listValues, int[] reversedValues)
+		{
+			var head = CreateLinkedList(listValues);
+			LinkedListItem<int> reversedHead = ProblemSolution<int>.Reverse(head);
+			Assert.IsNotNull(reversedHead);
+			int i = 0;
+			while (reversedHead != null)
+			{
+				Assert.That(reversedHead.Value, Is.EqualTo(reversedValues[i]));
+				reversedHead = reversedHead.Next;
+				i++;
+			}
+		}
+
+		private static LinkedListItem<T> CreateLinkedList<T>(T[] listValues)
+		{
+			var head = new LinkedListItem<T> {Value = listValues[0]};
+			LinkedListItem<T> p = head;
+			for (int i = 1; i < listValues.Length; i++)
+			{
+				p.Next = new LinkedListItem<T> {Value = listValues[i]};
+				p = p.Next;
+			}
+			return head;
+		}
+	}
+}
---
+++ InterviewQuestions.ReverseLinkedListProblem/ProblemSolution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace InterviewQuestions.ReverseLinkedListProblem
+{
+	public class LinkedListItem<T>
+	{
+		public T Value { get; set; }
+		public LinkedListItem<T> Next { get; set; }
+	}
+
+	public class ProblemSolution<T>
+	{
+		
+
+		public static LinkedListItem<T> Reverse(LinkedListItem<T> head)
+		{
+			if (head == null)
+				return null;
+
+			LinkedListItem<T> previous = null;
+			LinkedListItem<T> current = head;
+			while (current.Next != null)
+			{
+				LinkedListItem<T> t = current.Next;
+				current.Next = previous;
+				previous = current;
+				current = t;
+			}
+			return current;
+		}
+
+		[Obsolete]
+		public static LinkedListItem<T> ReverseFirstVersion(LinkedListItem<T> head)
+		{
+			if (head == null)
+				return null;
+
+			LinkedListItem<T> next = head.Next;
+			LinkedListItem<T> current = head;
+			while (next != null)
+			{
+				LinkedListItem<T> t = next.Next;
+				next.Next = current;
+				current = next;
+				next = t;
+			}
+			head.Next = null;
+			return current;
+		}
+	}
+}
