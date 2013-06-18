ab0f39f41831c11cdd9d2ee82b8a25d7243c5fc1
Alexander Kapelyukhovskiy <alkapa2005@gmail.com>
2013-06-18 18:04:53 +0300

    47. 7/15	Multiple a number by 7

---
+++ InterviewQuestions.MultipleNumberBySeven.Tests/ProblemSolutionTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+namespace InterviewQuestions.MultipleNumberBySeven.Tests
+{
+	[TestFixture]
+	public class ProblemSolutionTests
+	{
+		[Test]
+		[TestCase(4, 28)]
+		[TestCase(5, 35)]
+		public void MultipleNumberBySevenTest(int number, int expectedResult)
+		{
+			int result = ProblemSolution.MultipleBySeven(number);
+			Assert.That(result, Is.EqualTo(expectedResult));
+		}
+	}
+}
---
+++ InterviewQuestions.MultipleNumberBySeven/ProblemSolution.cs
@@ -0,0 +1,10 @@
+﻿namespace InterviewQuestions.MultipleNumberBySeven
+{
+	public class ProblemSolution
+	{
+		public static int MultipleBySeven(int number)
+		{
+			return (number << 3) - number;
+		}
+	}
+}
