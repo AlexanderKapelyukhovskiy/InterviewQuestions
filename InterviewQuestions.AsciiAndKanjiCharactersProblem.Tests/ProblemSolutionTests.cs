97d4e4b487f22d7fa4d2d51ed247b155ed17c29f
Alexander Kapelyukhovskiy <alkapa2005@gmail.com>
2013-07-25 17:21:57 +0300

    48.	7/20	ASCII and Kanji characters

---
+++ InterviewQuestions.AsciiAndKanjiCharactersProblem.Tests/ProblemSolutionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace InterviewQuestions.AsciiAndKanjiCharactersProblem.Tests
+{
+	[TestFixture]
+	public class ProblemSolutionTests
+	{
+		[Test]
+		[TestCase(1, new byte[] { 0, 1, 128 }, 2, false)]
+		[TestCase(2, new byte[] { 0, 127, 1 }, 2, true)]
+		[TestCase(3, new byte[] { 0, 128, 1 }, 2, false)]
+		[TestCase(4, new byte[] { 128, 128, 1 }, 2, true)]
+		[TestCase(5, new byte[] { 0, 128, 128, 1 }, 3, true)]
+		[TestCase(6, new byte[] {0, 128, 128, 128, 1 }, 4, false)]
+		[TestCase(7, new byte[] { 128, 128, 128, 128, 1 }, 4, true)]
+		[TestCase(8, new byte[] { 0, 1, 128 }, 0, true)]
+		public void IsAsciiTest(int testCaseIndex, byte[] data, int index, bool result )
+		{
+			Assert.That(ProblemSolution.IsAscii(data, index), Is.EqualTo(result));
+		}
+
+		[Test]
+		[TestCase(new byte[] { 0, 1, 128 }, -1)]
+		[TestCase(new byte[] { 0, 1, 128 }, 3)]
+		[ExpectedException(typeof(IndexOutOfRangeException))]
+		public void OutOutOfRangeTest(byte[] data, int index)
+		{
+			ProblemSolution.IsAscii(data, index);
+		}
+
+		[Test]
+		[ExpectedException]
+		[TestCase(new byte[] { 128, 1, 128 }, 0)]
+		public void InvalidDataTest(byte[] data, int index)
+		{
+			ProblemSolution.IsAscii(data, index);
+		}
+	}
+}
---
+++ InterviewQuestions.AsciiAndKanjiCharactersProblem/ProblemSolution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace InterviewQuestions.AsciiAndKanjiCharactersProblem
+{
+	public class ProblemSolution
+	{
+		public static bool IsAscii(byte[] data, int index)
+		{
+			if (index<0 || index>=data.Length)
+				throw new IndexOutOfRangeException();
+
+			const long msbMask = 1 << 7;// msb - 10000000
+
+			if (index == 0 && (data[index] & msbMask) != 0) // msb is 1 so this is not an ascii but there is not previous symbol
+				throw new Exception("Invalid data, single byte have msb equals to 1 ");
+
+			if ((data[index] & msbMask) != 0)//msb is 1 - so this is not acsii symbol
+				return false;
+
+			int currentIndex = index - 1;
+			while ((currentIndex) >= 0 && (data[currentIndex] & msbMask) != 0)
+			{
+				currentIndex--;
+			}
+
+			int countOfOnes = (index - currentIndex) - 1;
+			return (countOfOnes & 1) == 0;// if count of msb=1 is even then this is ascii
+		}
+	}
+}
