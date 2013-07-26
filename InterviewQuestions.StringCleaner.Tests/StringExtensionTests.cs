c053b885bd6a4df8b5c98cb5a3cee90c4b4c0bfb
Alexander Kapelyukhovskiy <alkapa2005@gmail.com>
2013-07-26 18:57:31 +0300

    52. 8/23  Diff of S1 and S2 strings

---
+++ InterviewQuestions.StringCleaner.Tests/StringExtensionTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+namespace InterviewQuestions.StringCleaner.Tests
+{
+	[TestFixture]
+	public class StringExtensionTests
+	{
+		[Test]
+		[TestCase("Hello Wborld", "b", "Hello World")]
+		public void RemoveTest(string s2, string s1, string result)
+		{
+			Assert.That(s2.Remove(s1), Is.EqualTo(result));
+		}
+	}
+}
---
+++ InterviewQuestions.StringCleaner/StringExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace InterviewQuestions.StringCleaner
+{
+	public static class StringExtension
+	{
+		public static string Remove(this string s2, string s1)
+		{
+			return null;
+		}
+	}
+}
